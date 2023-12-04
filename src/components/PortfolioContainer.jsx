c5605bba28e5e7dd8e8199cad0972538ce0069a4
David Wright <145091475+d-a-v-i-d-w-r-i-g-h-t@users.noreply.github.com>
2023-12-04 13:03:32 -0800

    Add PortfolioContainer

---
+++ src/components/PortfolioContainer.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+
+import NavTabs from './NavTabs';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Footer from './Footer';
+
+export default function PortfolioContainer() {
+  const [currentPage, setCurrentPage] = useState('Home');
+
+  const renderPage = () => {
+    if (currentPage === 'Home') {
+      return <Home />;
+    }
+    if (currentPage === 'About') {
+      return <About />;
+    }
+    if (currentPage === 'Contact') {
+      return <Contact />;
+    }
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
+  return (
+    <div>
+      <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
+
+      <main className="mx-3">{renderPage()}</main>
+
+      <Footer />
+    </div>
+  );
+}
