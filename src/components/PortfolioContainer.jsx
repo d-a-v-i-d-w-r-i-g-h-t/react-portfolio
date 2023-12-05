01785dfb69cb15bcc69e561904a7c15981e52123
David Wright <145091475+d-a-v-i-d-w-r-i-g-h-t@users.noreply.github.com>
2023-12-04 16:09:52 -0800

    Update PortfolioContainer

--- src/components/PortfolioContainer.jsx
+++ src/components/PortfolioContainer.jsx
@@ -1,23 +1,27 @@
 import { useState } from 'react';
 
 import NavTabs from './NavTabs';
-import Home from './pages/Home';
-import About from './pages/About';
+import AboutMe from './pages/AboutMe';
+import Portfolio from './pages/Portfolio';
 import Contact from './pages/Contact';
+import Resume from './pages/Resume';
 import Footer from './Footer';
 
 export default function PortfolioContainer() {
   const [currentPage, setCurrentPage] = useState('Home');
 
   const renderPage = () => {
-    if (currentPage === 'Home') {
-      return <Home />;
+    if (currentPage === 'AboutMe') {
+      return <AboutMe />;
     }
-    if (currentPage === 'About') {
-      return <About />;
+    if (currentPage === 'Portfolio') {
+      return <Portfolio />;
     }
     if (currentPage === 'Contact') {
       return <Contact />;
+    }
+    if (currentPage === 'Resume') {
+      return <Resume />;
     }
   };
 
