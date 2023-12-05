4d4b5cd9b0683ae3877d918d115774cf73870f05
David Wright <145091475+d-a-v-i-d-w-r-i-g-h-t@users.noreply.github.com>
2023-12-04 17:36:42 -0800

    Add Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <div>
+      <p><a href="https://github.com/d-a-v-i-d-w-r-i-g-h-t">GitHub</a></p>
+      <p><a href="https://www.linkedin.com/in/davidspencerwright/">LinkedIn</a></p>
+      <p><a href="https://medium.com/@d-a-v-i-d-w-r-i-g-h-t">Medium</a></p>
+    </div>
+  )
+}
