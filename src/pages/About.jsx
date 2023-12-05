095863233364f5193dcbd8fceee18c7b140fedb5
David Wright <145091475+d-a-v-i-d-w-r-i-g-h-t@users.noreply.github.com>
2023-12-04 16:59:27 -0800

    Add image to About page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,18 +2,12 @@ export default function About() {
   return (
     <div>
       <h1>About Page</h1>
+      <img src="../assets/images/David-Profile-Photo.jpeg" alt="David Wright" />
       <p>
-        Integer sed justo eu nibh pellentesque tristique. Ut ut leo ac
-        justo auctor vulputate. Aenean aliquet ipsum eu tincidunt congue.
-        Sed semper, felis ut scelerisque feugiat, mauris mauris euismod
-        tortor, ut dapibus odio mauris eu lacus. Sed ut consectetur erat.
-        Fusce tempor cursus purus, eu sollicitudin justo volutpat ac.
-        Integer a augue nec elit ultricies fermentum. Integer auctor elit
-        sed ante dignissim congue. Curabitur at odio ac elit fermentum
-        pharetra ac id felis. Aenean semper euismod ex vitae fringilla.
-        Proin id neque leo. Duis tristique elit ut nibh bibendum, vel luctus 
-        acus malesuada. Proin a libero et erat dapibus tristique.
+        I'm an aerospace engineer and full-stack web developer
+        from Portland, Oregon, transplanted to the Silicon Valley.
+        Tech has always been a fascination for me, from software to satellites.
       </p>
     </div>
-  )
+  );
 }
