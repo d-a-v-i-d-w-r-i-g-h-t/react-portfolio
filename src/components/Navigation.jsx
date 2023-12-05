6f264026144a3f1d79bc8a6a9e547d927d855928
David Wright <145091475+d-a-v-i-d-w-r-i-g-h-t@users.noreply.github.com>
2023-12-04 16:47:57 -0800

    Add Navigation.jsx code

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,9 +1,50 @@
-function Naviagation({ currentPage, handlePageChange }) {
+// dependencies from react-router-dom
+import { Link, useLocation } from 'react-router-dom';
+
+function Navigation() {
+  const currentPage = useLocation().pathname;
+
   return (
     <ul className="nav nav-tabs">
-      <li>
-        <a href=""></a>
+
+      <li className="nav-item">
+        <Link
+          to="/"
+          className={currentPage === '/' ? 'nav-link active' : 'nav-link'}
+        >
+          About Me
+        </Link>
       </li>
+
+      <li className="nav-item">
+        <Link
+          to="/Portfolio"
+          className={currentPage === '/Portfolio' ? 'nav-link active' : 'nav-link'}
+        >
+          Portfolio
+        </Link>
+      </li>
+
+      <li className="nav-item">
+        <Link
+          to="/Contact"
+          className={currentPage === '/Contact' ? 'nav-link active' : 'nav-link'}
+        >
+          Contact
+        </Link>
+      </li>
+
+      <li className="nav-item">
+        <Link
+          to="/Resume"
+          className={currentPage === '/Resume' ? 'nav-link active' : 'nav-link'}
+        >
+          Resume
+        </Link>
+      </li>
+      
     </ul>
-  )
+  );
 }
+
+export default NavTabs;
