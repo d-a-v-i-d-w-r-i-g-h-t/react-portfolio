2ac001e106a7405135f83408edd0ff10f69cc462
David Wright <145091475+d-a-v-i-d-w-r-i-g-h-t@users.noreply.github.com>
2023-12-04 17:39:09 -0800

    Add Header.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import Navigation from './Navigation';
+
+export default function Header() {
+  return (
+    <>
+      <div>
+        <p>David Wright</p>
+      </div>
+      <div>
+        <Navigation />
+      </div>
+    </>
+  )
+}
