afad006360f3e0ef04b3a2dfcae05251469203f2
David Wright <145091475+d-a-v-i-d-w-r-i-g-h-t@users.noreply.github.com>
2023-12-04 17:07:07 -0800

    Add Resume page placeholder

---
+++ src/pages/Resume.jsx
@@ -0,0 +1,19 @@
+export default function Resume() {
+  return (
+    <div>
+      <h1>Resume Page</h1>
+      <p>Download my resume <a href="">here</a></p>
+      <h2>Proficiencies</h2>
+      <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JavaScript</li>
+        <li>MySQL</li>
+        <li>MongoDB</li>
+        <li>Express.js</li>
+        <li>React</li>
+        <li>Node.js</li>
+      </ul>
+    </div>
+  );
+}
