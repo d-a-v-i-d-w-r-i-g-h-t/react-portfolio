46b92fd62fd7e44ed106638ed93bb43036bec813
David Wright <145091475+d-a-v-i-d-w-r-i-g-h-t@users.noreply.github.com>
2023-12-04 17:03:05 -0800

    Add Contact.jsx placeholder

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,9 @@
+export default function Contact() {
+  return (
+    <div>
+      <h1>Contact Page</h1>
+      <p>Contact Form</p>
+      <p><a href="mailto:d4v1dwr1gh7@gmail.com">d4v1dwr1gh7@gmail.com</a></p>
+    </div>
+  );
+}
