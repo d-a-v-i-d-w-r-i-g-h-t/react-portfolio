ba6743ca1f37b4d9c2be6647f4a22da41bec6af2
David Wright <145091475+d-a-v-i-d-w-r-i-g-h-t@users.noreply.github.com>
2023-12-04 16:54:47 -0800

    Add About.jsx placeholder

---
+++ src/pages/About.jsx
@@ -0,0 +1,19 @@
+export default function About() {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>
+        Integer sed justo eu nibh pellentesque tristique. Ut ut leo ac
+        justo auctor vulputate. Aenean aliquet ipsum eu tincidunt congue.
+        Sed semper, felis ut scelerisque feugiat, mauris mauris euismod
+        tortor, ut dapibus odio mauris eu lacus. Sed ut consectetur erat.
+        Fusce tempor cursus purus, eu sollicitudin justo volutpat ac.
+        Integer a augue nec elit ultricies fermentum. Integer auctor elit
+        sed ante dignissim congue. Curabitur at odio ac elit fermentum
+        pharetra ac id felis. Aenean semper euismod ex vitae fringilla.
+        Proin id neque leo. Duis tristique elit ut nibh bibendum, vel luctus 
+        acus malesuada. Proin a libero et erat dapibus tristique.
+      </p>
+    </div>
+  )
+}
