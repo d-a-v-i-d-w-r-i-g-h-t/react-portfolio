bc3ef173c59616579793600ed9fec632f0c53508
David Wright <145091475+d-a-v-i-d-w-r-i-g-h-t@users.noreply.github.com>
2023-12-06 04:13:08 -0800

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,5 @@
 import express from 'express';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+import path from 'path';
 
 const app = express();
 
