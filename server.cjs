e0533c7e8cf586dafec676ac3e7fe61eb349e606
David Wright <145091475+d-a-v-i-d-w-r-i-g-h-t@users.noreply.github.com>
2023-12-06 04:17:10 -0800

    Update server.cjs file

--- server.cjs
+++ server.cjs
@@ -1,8 +1,7 @@
-import express from 'express';
-import path from 'path';
+const express = require('express');
+const path = require('path');
 
 const app = express();
-
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
