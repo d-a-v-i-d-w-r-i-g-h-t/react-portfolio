3bbf5ad8ace67417734160c894f456dd3bae1124
David Wright <145091475+d-a-v-i-d-w-r-i-g-h-t@users.noreply.github.com>
2023-12-06 02:26:00 -0800

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
-const express = require('express');
-const path = require('path');
-const app = express();
+import express from 'express';
+import path from 'path';
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
